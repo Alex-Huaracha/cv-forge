272ed6bb8565d5087069736fa2b6be01665a8007
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-10-15 14:12:37 -0500

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* Header */}
+      <header className="header">
+        <h2>CV Forge</h2>
+        <p>Your CV quickly and easily</p>
+      </header>
+    </>
+  );
 }
 
 export default App;
