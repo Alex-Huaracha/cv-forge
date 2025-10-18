59d99b9848e50cde7f139e168e96598c64ffeb39
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-10-18 09:19:31 -0500

    feat: add Trash Icon to icons.svg; refactor ExperienceSection to use ExperienceItem component for rendering items

--- src/components/ExperienceSection/ExperienceSection.jsx
+++ src/components/ExperienceSection/ExperienceSection.jsx
@@ -1,3 +1,6 @@
+import { ExperienceItem } from './../../components';
+import './ExperienceSection.css';
+
 export function ExperienceSection({
   items,
   onUpdateItems,
@@ -11,12 +14,9 @@ export function ExperienceSection({
   };
 
   const handleChange = (id, fieldName, value) => {
-    const updatedItems = items.map((item) => {
-      if (item.id === id) {
-        return { ...item, [fieldName]: value };
-      }
-      return item;
-    });
+    const updatedItems = items.map((item) =>
+      item.id === id ? { ...item, [fieldName]: value } : item
+    );
     onUpdateItems(updatedItems);
   };
 
@@ -27,48 +27,18 @@ export function ExperienceSection({
 
   return (
     <div className="experience-section">
-      <h3>{sectionTitle}</h3>
-
       {items.map((item) => (
-        <div key={item.id} className="entry-form">
-          {fields.map((field) => (
-            <div key={field.name} className="form-group">
-              <label htmlFor={`${item.id}-${field.name}`}>{field.label}</label>
-              {field.type === 'textarea' ? (
-                <textarea
-                  id={`${item.id}-${field.name}`}
-                  placeholder={field.label}
-                  value={item[field.name]}
-                  onChange={(e) =>
-                    handleChange(item.id, field.name, e.target.value)
-                  }
-                  rows="4"
-                />
-              ) : (
-                <input
-                  type={field.type}
-                  id={`${item.id}-${field.name}`}
-                  placeholder={field.label}
-                  value={item[field.name]}
-                  onChange={(e) =>
-                    handleChange(item.id, field.name, e.target.value)
-                  }
-                />
-              )}
-            </div>
-          ))}
-
-          <button
-            onClick={() => handleDelete(item.id)}
-            className="delete-button"
-          >
-            Delete
-          </button>
-        </div>
+        <ExperienceItem
+          key={item.id}
+          item={item}
+          fields={fields}
+          onDelete={handleDelete}
+          onUpdate={handleChange}
+        />
       ))}
 
       <button onClick={handleAdd} className="add-button">
-        Add {sectionTitle}
+        + Add {sectionTitle}
       </button>
     </div>
   );
