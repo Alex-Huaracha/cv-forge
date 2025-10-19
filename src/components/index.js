ea64fe43a0c067c21e74dc9f0bec495aa114037c
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-10-19 17:44:19 -0500

    feat: add ResumePreview component with styling and structure for CV

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export * from './AccordionSection/AccordionSection';
 export * from './PersonalInfo/PersonalInfo';
 export * from './ExperienceSection/ExperienceSection';
 export * from './AccordionItem/AccordionItem';
+export * from './ResumePreview/ResumePreview';
