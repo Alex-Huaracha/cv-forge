e11de483fd8bd19c0ffd04294363e5d5d41e2008
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-10-22 16:38:33 -0500

    fix: Fix Vite config for react-pdf/renderer with pako dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@react-pdf/renderer': '@react-pdf/renderer/dist/react-pdf.es.js',
-    },
-  },
 });
